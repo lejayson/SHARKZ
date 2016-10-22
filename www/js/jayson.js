1b986d4296cb1911fd2932ea3c359e3ef32bd39c
lejayson <jayson_lee@live.com>
2016-10-21 23:23:12 -1000

    Jayson branch (#7)
    * jayson edit
    
    edit to sass
    
    * Jayson Update
    
    Integrated Firebase added custom js files (see jayson.js and
    newentry.html for reference)
    
    * Jayson edit
    
    Add function applied to rough form

--- www/js/jayson.js
+++ www/js/jayson.js
@@ -1,4 +1,11 @@
-function jaysonCtrl($scope, $compile, $http, $timeout) {
-	console.log("jayson being called");
+function jaysonCtrl($scope, $firebaseObject, $firebaseArray, $compile, $http, $timeout) {
+  $scope.client = {};
+  var rootRef = firebase.database().ref();
+  var arrRef = rootRef.child('JaysonSampleClient');
+  $scope.testvar = $firebaseArray(arrRef);
+  $scope.addClient = function () {
+    $scope.testvar.$add({name: $scope.client.name, gender:$scope.client.gender}).then(function(ref) {});
+    $scope.client.name = null;
+  }
 }
-angular.module("SHARKZ").controller("jaysonCtrl", ["$scope", "$compile", "$http", "$timeout", jaysonCtrl]);
+angular.module("SHARKZ").controller("jaysonCtrl", ["$scope", "$firebaseObject", "$firebaseArray", "$compile", "$http", "$timeout", jaysonCtrl]);
