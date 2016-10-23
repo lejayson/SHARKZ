1469fd9e9c063c4866ed313c54e3c98a0aaebb27
kloo <tehpiratezor@gmail.com>
2016-10-22 20:32:47 -1000

    Database write on bed assignment

--- www/js/setShelter.js
+++ www/js/setShelter.js
@@ -1,13 +1,43 @@
 function setShelterCtrl($scope, $stateParams, $firebaseObject, $firebaseArray, $ionicModal, $ionicSlideBoxDelegate ,$compile, $http, $timeout) {
   var rootRef = firebase.database().ref();
-  var arrRef = rootRef.child('agencies');
+  var arrRef = rootRef.child('Assigned_bed');
   var tblRef;
   var arrVariable;
   $scope.Shelters = $firebaseArray(arrRef);
   
-  $scope.setShelter = function(id, index){
-    tblRef = rootRef.child('agencies').child(id).child($scope.Shelters[index].ID);
-    tblRef.set({Service: $scope.Shelters[index].Service, Description: $scope.Shelters[index].Description});
+  $scope.setShelter = function(id, clientid){
+    
+    var privCMRef = arrRef.orderByChild("Bed_ID").equalTo(id);
+    
+    $scope.privCM = $firebaseArray(privCMRef);
+    
+    $scope.privCM.$loaded().then(function() {
+      $scope.privCM[0].User_ID = clientid;
+      $scope.privCM.$save(0).then(function(ref) {
+        ref.key === $scope.privCM[0].$id; // true
+      });
+    });
+    
+    $scope.closeSetShelterPanel;
+    
+    console.log(id + " " + clientid);
+  }
+  
+  $scope.unsetShelter = function(id, clientid){
+    var privCMRef = arrRef.orderByChild("Bed_ID").equalTo(id);
+    
+    $scope.privCM = $firebaseArray(privCMRef);
+    
+    $scope.privCM.$loaded().then(function() {
+      $scope.privCM[0].User_ID = 0;
+      $scope.privCM[0].occupy = 0;
+      $scope.privCM.$save(0).then(function(ref) {
+        ref.key === $scope.privCM[0].$id; // true
+      });
+    });
+    
+    $scope.closeSetShelterPanel;
+    
   }
 };
 
